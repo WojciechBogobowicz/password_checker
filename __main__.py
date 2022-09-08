76798ab6e39720fd93c28384f9e29f8579ce0a20
WojciechBogobowicz <32848549+WojciechBogobowicz@users.noreply.github.com>
2022-09-08 10:24:39 +0200

    information about breaked rules added

--- __main__.py
+++ __main__.py
@@ -7,10 +7,17 @@ from rules_specification import *
 def main():
     app = Flask(__name__)
     validator = PasswordValidator(specified_rules)
+
+
     @app.route('/get-strong-password', methods=['POST'])
     def index():
         data = request.data.decode()
-        return validator.get_fixed_password(data)
+        rules_that_dont_passed = validator.get_invalid_rule_names(data)
+        if rules_that_dont_passed:
+            return f"""Your password don't satissify folowing rules: {rules_that_dont_passed}.
+            Maybe, you want use '{validator.get_fixed_password(data)}' as your password instead?"""
+        return "Everything is okay with your password."
+
 
     app.run()
 
--- validator.py
+++ validator.py
@@ -9,14 +9,23 @@ class PasswordValidator:
         self.rules = [self.factory.create_instance(rule_name, **kwargs) for rule_name, kwargs in self.rules_dict.items()]
             
     def get_fixed_password(self, passwd):
+        current_try = 0
         while not self._is_password_valid(passwd):
             for rule in self.rules:
                 passwd = rule.fix_validation_issue_if_needed(passwd)
         return passwd
-
 
     def _is_password_valid(self, passwd):
         validation_flags = []
         for rule in self.rules:
             validation_flags.append(rule.is_validated(passwd))
         return all(validation_flags)
+    
+    def get_invalid_rule_names(self, passwd):
+        acc = []
+        for rule in self.rules:
+            if not rule.is_validated(passwd):
+                acc.append(rule.__class__.__name__)
+        return ", ".join(acc)
+
+    # class 
