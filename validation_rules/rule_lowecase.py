ce757a14e2616002ac54c459b7f5ab62be8c7d96
WojciechBogobowicz <32848549+WojciechBogobowicz@users.noreply.github.com>
2022-09-08 09:04:22 +0200

    Create rule_lowecase.py
    without tests

---
+++ validation_rules/rule_lowecase.py
@@ -0,0 +1,16 @@
+from .rule_abs import AbsValidationRule
+import string
+import random
+from .uttils import random_swap
+
+class UpercaseRule(AbsValidationRule):
+    def is_validated(self, text: str) -> bool:
+        return self._contains_lowecase(text)
+    
+    def _contains_lowercase(self, text):
+        return any(letter.islower() for letter in text)
+    
+    def fix_validation_issue_if_needed(self, text):
+        if self.is_validated(text):
+            return text
+        return ''.join(random_swap(text, random.choice(string.ascii_uppercase)))
