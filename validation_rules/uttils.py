b3cb89baf4081ce3f37e1d669aac1baab0415a1a
WojciechBogobowicz <32848549+WojciechBogobowicz@users.noreply.github.com>
2022-09-08 08:29:55 +0200

    generate_random_password added to utils

--- validation_rules/uttils.py
+++ validation_rules/uttils.py
@@ -8,4 +8,18 @@ def random_swap(item1, item2, proba=0.1):
     if random.random() < proba:
         return item1, item2
     return item2, item1
-    
+
+def genrate_random_password(
+        length: int, 
+        with_lowercase: bool = True,
+        with_uppercase: bool = True,
+        with_digits: bool = True,
+        with_specialcharacters: bool = True
+    ):
+    base_characters = ''.join([
+        string.ascii_lowercase if with_lowercase else '',
+        string.ascii_uppercase if with_uppercase else '',
+        string.digits if with_digits else '',
+        SPECIAL_CHARACTERS if with_specialcharacters else ''
+    ])
+    return ''.join([random.choice(base_characters) for _ in range(length)])
