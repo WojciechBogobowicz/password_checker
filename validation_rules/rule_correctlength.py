4a0d947366631cc193b3cb23358ed75ef10765b6
WojciechBogobowicz <32848549+WojciechBogobowicz@users.noreply.github.com>
2022-09-08 08:48:16 +0200

    Update rule_correctlength.py
    CorrectLength.fix_validation_issue_if_needed add generate more random sufix to password.

--- validation_rules/rule_correctlength.py
+++ validation_rules/rule_correctlength.py
@@ -3,6 +3,7 @@ from math import inf
 import unittest
 import random
 import string
+from .uttils import genrate_random_password
 
 
 class CorrectLength(AbsValidationRule):
@@ -19,7 +20,8 @@ class CorrectLength(AbsValidationRule):
         if len(text) > self.maximal_length:
             return text[len(text) - self.minimal_length:]
         elif len(text) < self.minimal_length:
-            return text + ''.join(random.choice(string.ascii_lowercase) for i in range(self.minimal_length - len(text)))
+            missing_len = self.minimal_length - len(text)
+            return text + genrate_random_password(missing_len)
         return text
 
 
